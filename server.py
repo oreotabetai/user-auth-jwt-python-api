f5482c0650306eff7cf05e0f69404039528a309e
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-10-30 23:54:34 +0900

    define each endpoint

--- server.py
+++ server.py
@@ -9,3 +9,11 @@ def read_root():
 @app.post("/login")
 def login_user():
     return {"return jwt if authorize"}
+
+@app.post("/register")
+def register_user():
+    return {"register user"}
+
+@app.get("/users/{user_id}")
+def get_info(user_id: int):
+    return {"return user " + str(user_id) + "information"}
