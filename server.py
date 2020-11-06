f68225e37b5c4bc9ae197b589ce48798a170e1dc
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-11-06 15:41:30 +0900

    fix import path

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Depends
 from pydantic import BaseModel
-from src.auth import authenticate, get_current_user
-from src.user import fetch_user_info, create_user
+from src import auth, user
 from src.model.user import UserTable
 
 class User(BaseModel):
@@ -16,12 +15,12 @@ def read_root():
 
 @app.post("/login")
 async def login(user: User):
-  return authenticate(user.name, user.password)
+  return auth.authenticate(user.name, user.password)
 
 @app.post("/register")
 async def register_user():
-  return create_user()
+  return user.create_user()
 
 @app.get("/users/me")
-async def get_info(current_user: UserTable = Depends(get_current_user)):
+async def get_info(current_user: UserTable = Depends(auth.get_current_user)):
   return current_user
--- src/user.py
+++ src/user.py
@@ -1,14 +1,12 @@
-import sys
-sys.path.append('../')
-from db import session
-from model.user import UserTable
+from .db import session
+from .model.user import UserTable
 
 def fetch_user_info(user_id: str):
   user = session.query(UserTable).filter(UserTable.id == user_id).first()
   return user
 
 def search_user(username: str):
-  return session.query(UserTable).filter(UserTable.name == username).first()
+  return session.query(UserTable).filter(UserTable.id == username).first()
 
 def create_user():
   return {"register user"}
