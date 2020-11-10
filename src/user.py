c5d10a62769f6011275e17281bda143b967deebd
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-11-10 19:45:10 +0900

    functions for create user

--- src/user.py
+++ src/user.py
@@ -1,12 +1,23 @@
 from .db import session
-from .model.user import UserTable
+from .model.user import UserTable, CreateUser
+from fastapi import HTTPException
+import bcrypt
 
 def fetch_user_info(user_id: str):
   user = session.query(UserTable).filter(UserTable.id == user_id).first()
   return user
 
-def search_user(username: str):
-  return session.query(UserTable).filter(UserTable.id == username).first()
+def search_user(userID: str):
+  return session.query(UserTable).filter(UserTable.id == userID).first()
 
-def create_user():
-  return {"register user"}
+def create_user(userID: str, name: str, pass_hash: str, email: str):
+  user = UserTable()
+  if search_user(userID):
+    raise HTTPException(status_code=409, detail="UserID is already registered")
+  user.id = userID
+  user.name = name
+  user.password_hash = pass_hash
+  user.email = email
+  session.add(user)
+  session.commit()
+  return {"successed registering user!"}
